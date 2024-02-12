6816714d76210201b0c74d4016ee21b99d3c0aa5
Alexander Berryhill <amsb99@gmail.com>
2024-02-11 18:04:40 -0700

    Reorganized Arduino code

--- wheelchair_arduino.ino
+++ wheelchair_arduino.ino
@@ -1,123 +1,123 @@
-// Motor driver 1 pins
-const int motor1Pin1 = 2;
-const int motor1Pin2 = 3;
-const int motor2Pin1 = 4;
-const int motor2Pin2 = 5;
-
-// Motor driver 2 pins
-const int motor3Pin1 = 6;
-const int motor3Pin2 = 7;
-const int motor4Pin1 = 8;
-const int motor4Pin2 = 9;
-
-// Serial input
-const int serialInputPin = 10;
+// Define motor control pins
+#define MOTOR1_PIN1 2
+#define MOTOR1_PIN2 3
+#define MOTOR2_PIN1 4
+#define MOTOR2_PIN2 5
+#define MOTOR3_PIN1 6
+#define MOTOR3_PIN2 7
+#define MOTOR4_PIN1 8
+#define MOTOR4_PIN2 9
 
 void setup() {
-  // Motor driver 1 pins as outputs
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(motor2Pin1, OUTPUT);
-  pinMode(motor2Pin2, OUTPUT);
+  // Set all the motor control pins to outputs
+  pinMode(MOTOR1_PIN1, OUTPUT);
+  pinMode(MOTOR1_PIN2, OUTPUT);
+  pinMode(MOTOR2_PIN1, OUTPUT);
+  pinMode(MOTOR2_PIN2, OUTPUT);
+  pinMode(MOTOR3_PIN1, OUTPUT);
+  pinMode(MOTOR3_PIN2, OUTPUT);
+  pinMode(MOTOR4_PIN1, OUTPUT);
+  pinMode(MOTOR4_PIN2, OUTPUT);
 
-  // Motor driver 2 pins as outputs
-  pinMode(motor3Pin1, OUTPUT);
-  pinMode(motor3Pin2, OUTPUT);
-  pinMode(motor4Pin1, OUTPUT);
-  pinMode(motor4Pin2, OUTPUT);
-
-  // Serial communication
-  Serial.begin(9600);
+  Serial.begin(9600);  // Start serial communication at 9600 bps
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char serialInput = Serial.read(); // Read a single character from the serial input
-    
-    // Perform actions based on serial input
-    if (serialInput == '0') {
-      // Move the car
-      stopCar();
-    } else if (serialInput == 'c') {
-      // Stop the car
-      moveCar();
-    } else if (serialInput == 'l') {
-      // Stop the car
-      turnLeft();
-    } else if (serialInput == 'r') {
-      // Stop the car
-      turnRight();
-    }
+  if (Serial.available()) {  // If data is available to read
+    char val = Serial.read();  // Read it
+    move(val);
   }
-  
-  // Check for directional commands
-//  if (Serial.available() > 0) {
-//    String direction = Serial.readString(); // Read the direction from serial input
-//    
-//    // Adjust car's direction based on the received command
-//    if (direction == "left") {
-//      turnLeft();
-//    } else if (direction == "right") {
-//      turnRight();
-//    } else if (direction == "center") {
-//      moveCar();
-//    }
-//  }
 }
 
-// Function to move the car forward
-void moveCar() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  
-  digitalWrite(motor3Pin1, HIGH);
-  digitalWrite(motor3Pin2, LOW);
-  
-  digitalWrite(motor4Pin1, HIGH);
-  digitalWrite(motor4Pin2, LOW);
-}
-
-// Function to stop the car
-void stopCar() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW);
-  digitalWrite(motor3Pin1, LOW);
-  digitalWrite(motor3Pin2, LOW);
-  digitalWrite(motor4Pin1, LOW);
-  digitalWrite(motor4Pin2, LOW);
-}
-
-// Function to turn the car left
-void turnRight() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-  
-  digitalWrite(motor3Pin1, LOW);
-  digitalWrite(motor3Pin2, HIGH);
-  
-  digitalWrite(motor4Pin1, HIGH);
-  digitalWrite(motor4Pin2, LOW);
-}
-
-// Function to turn the car right
-void turnLeft() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
-  
-  digitalWrite(motor3Pin1, HIGH);
-  digitalWrite(motor3Pin2, LOW);
-  
-  digitalWrite(motor4Pin1, LOW);
-  digitalWrite(motor4Pin2, HIGH);
+void move(char command) {
+  switch(command) {
+    case '0':  // Stop
+      // All motors are stopped
+      /*
+        [Motor1]   [Motor2]
+            X         X
+             ----------
+             |        |
+             |  Car   |
+             |        |
+             ----------
+        [Motor3]   [Motor4]
+            X         X
+      */
+      digitalWrite(MOTOR1_PIN1, LOW);
+      digitalWrite(MOTOR1_PIN2, LOW);
+      digitalWrite(MOTOR2_PIN1, LOW);
+      digitalWrite(MOTOR2_PIN2, LOW);
+      digitalWrite(MOTOR3_PIN1, LOW);
+      digitalWrite(MOTOR3_PIN2, LOW);
+      digitalWrite(MOTOR4_PIN1, LOW);
+      digitalWrite(MOTOR4_PIN2, LOW);
+      break;
+    case 'l':  // Left
+      // Motors on the left side (Motor1 and Motor3) are moving forward, and motors on the right side (Motor2 and Motor4) are stopped
+      /*
+        [Motor1]   [Motor2]
+            ↑         X
+             ----------
+             |        |
+             |  Car   |
+             |        |
+             ----------
+        [Motor3]   [Motor4]
+            ↑         X
+      */
+      digitalWrite(MOTOR1_PIN1, HIGH);
+      digitalWrite(MOTOR1_PIN2, LOW);
+      digitalWrite(MOTOR2_PIN1, LOW);
+      digitalWrite(MOTOR2_PIN2, HIGH);
+      digitalWrite(MOTOR3_PIN1, HIGH);
+      digitalWrite(MOTOR3_PIN2, LOW);
+      digitalWrite(MOTOR4_PIN1, LOW);
+      digitalWrite(MOTOR4_PIN2, HIGH);
+      break;
+    case 'r':  // Right
+      // Motors on the right side (Motor2 and Motor4) are moving forward, and motors on the left side (Motor1 and Motor3) are stopped
+      /*
+        [Motor1]   [Motor2]
+            X         ↑
+             ----------
+             |        |
+             |  Car   |
+             |        |
+             ----------
+        [Motor3]   [Motor4]
+            X         ↑
+      */
+      digitalWrite(MOTOR1_PIN1, LOW);
+      digitalWrite(MOTOR1_PIN2, HIGH);
+      digitalWrite(MOTOR2_PIN1, HIGH);
+      digitalWrite(MOTOR2_PIN2, LOW);
+      digitalWrite(MOTOR3_PIN1, LOW);
+      digitalWrite(MOTOR3_PIN2, HIGH);
+      digitalWrite(MOTOR4_PIN1, HIGH);
+      digitalWrite(MOTOR4_PIN2, LOW);
+      break;
+    case 'c':  // Straight
+      // All motors are moving forward
+      /*
+        [Motor1]   [Motor2]
+            ↑         ↑
+             ----------
+             |        |
+             |  Car   |
+             |        |
+             ----------
+        [Motor3]   [Motor4]
+            ↑         ↑
+      */
+      digitalWrite(MOTOR1_PIN1, HIGH);
+      digitalWrite(MOTOR1_PIN2, LOW);
+      digitalWrite(MOTOR2_PIN1, HIGH);
+      digitalWrite(MOTOR2_PIN2, LOW);
+      digitalWrite(MOTOR3_PIN1, HIGH);
+      digitalWrite(MOTOR3_PIN2, LOW);
+      digitalWrite(MOTOR4_PIN1, HIGH);
+      digitalWrite(MOTOR4_PIN2, LOW);
+      break;
+  }
 }
